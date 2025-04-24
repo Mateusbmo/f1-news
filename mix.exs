7ecc7fe6b40d6326f72dc441adf653a17232ab97
mateusbmo <mateus.martins@cosmobots.io>
2025-04-24 15:06:03 -0300

    Fix config.exs for prod, update esbuild to 0.23.1, apply app.css changes, stabilize mix.exs, and add fly.toml and .dockerignore

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule F1News.MixProject do
     [
       app: :f1_news,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -13,9 +13,6 @@ defmodule F1News.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {F1News.Application, []},
@@ -23,59 +20,43 @@ defmodule F1News.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.20"},
-      {:phoenix_ecto, "~> 4.5"},
-      {:ecto_sql, "~> 3.10"},
+      {:bcrypt_elixir, "~> 3.2"},
+      {:phoenix, "~> 1.7.21"},
+      {:phoenix_ecto, "~> 4.6.3"},
+      {:ecto_sql, "~> 3.12.1"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0.0"},
+      {:phoenix_html, "~> 4.2"},
+      {:phoenix_live_reload, "~> 1.6.0", only: :dev},
+      {:phoenix_live_view, "~> 1.0.10"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
-      {:heroicons,
-       github: "tailwindlabs/heroicons",
-       tag: "v2.1.1",
-       sparse: "optimized",
-       app: false,
-       compile: false,
-       depth: 1},
-      {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
-      {:httpoison, "~> 2.0"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:phoenix_live_dashboard, "~> 0.8.6"},
+      {:esbuild, "~> 0.9.0", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.3.1", runtime: Mix.env() == :dev},
+      {:swoosh, "~> 1.18.4"},
+      {:finch, "~> 0.19.0"},
+      {:telemetry_metrics, "~> 1.1.0"},
+      {:telemetry_poller, "~> 1.2.0"},
+      {:gettext, "~> 0.26.2"},
+      {:jason, "~> 1.4.4"},
+      {:httpoison, "~> 2.2.2"},
+      {:bandit, "~> 1.6.11"},
+      {:comeonin, "~> 5.5.1"},
+      {:dns_cluster, "~> 0.1.3"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.setup": ["tailwind.install", "esbuild.install"],
       "assets.build": ["tailwind f1_news", "esbuild f1_news"],
       "assets.deploy": [
         "tailwind f1_news --minify",
