6fe4930ba8b2f0ea35c707179d892371ce0fd82e
mateusbmo <mateus.martins@cosmobots.io>
2025-04-25 13:44:59 -0300

    Move F1News.Repo config back to runtime.exs, remove from prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,16 +1,5 @@
 import Config
 
-# Database configuration
-config :f1_news, F1News.Repo,
-  url: System.get_env("DATABASE_URL") || raise("environment variable DATABASE_URL is missing"),
-  database: "f1kiyoshi",
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true,
-  ssl_opts: [
-    verify: :verify_peer,
-    cacerts: :castore.cacerts()
-  ]
-
 # Cache static assets in production
 config :f1_news, F1NewsWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
